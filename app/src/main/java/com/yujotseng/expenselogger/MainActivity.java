6cf5530473c9105fb84c9aff676456845cb134ee
Yu Jo <zoey5538@gmail.com>
2017-08-27 19:49:15 +0200

    Change to Bottom Navigation View

--- app/src/main/java/com/yujotseng/expenselogger/MainActivity.java
+++ app/src/main/java/com/yujotseng/expenselogger/MainActivity.java
@@ -1,24 +1,47 @@
 package com.yujotseng.expenselogger;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private SectionsPagerAdapter sectionPagerAdapter;
-
-    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sectionPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        return true;
+                    case R.id.action_calendar:
+                        return true;
+                    case R.id.action_analysis:
+                        return true;
+                    case R.id.action_settings:
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+        /*sectionPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         viewPager = (ViewPager) findViewById(R.id.container);
 
@@ -30,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_date_range_black_24dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_assessment_black_24dp);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_black_24dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_black_24dp);*/
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    /*private void setupViewPager(ViewPager viewPager) {
         SectionsPagerAdapter spAdapter = new SectionsPagerAdapter((getSupportFragmentManager()));
 
         spAdapter.addFragment(new CalendarFragment());
@@ -41,5 +64,5 @@ public class MainActivity extends AppCompatActivity {
         spAdapter.addFragment(new SettingsFragment());
 
         viewPager.setAdapter(spAdapter);
-    }
+    }*/
 }
