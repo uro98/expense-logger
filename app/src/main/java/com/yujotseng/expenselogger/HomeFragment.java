058d52035740688b4e91582ae9e57bbc0abd0bb6
Yu Jo <zoey5538@gmail.com>
2017-08-28 22:38:10 +0200

    Create database, add addExpense functionality and show expenseList in ListView

--- app/src/main/java/com/yujotseng/expenselogger/HomeFragment.java
+++ app/src/main/java/com/yujotseng/expenselogger/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.yujotseng.expenselogger;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -8,26 +10,41 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
 
     private View view;
-    Fragment fragment;
+    private Button newEntryButton;
+    private Fragment fragment;
+    private DatabaseHandler databaseHandler;
+    private ListView expenseListView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.home_layout, container, false);
 
-        Button newEntryButton = (Button) view.findViewById(R.id.newEntryButton);
+        // Instantiate database
+        databaseHandler = new DatabaseHandler(getActivity(), null, null, 1);
+
+        newEntryButton = (Button) view.findViewById(R.id.newEntryButton);
 
         fragment = getFragmentManager().findFragmentByTag("NewEntryFragment");
 
         if(fragment == null) {
             fragment = new NewEntryFragment();
         }
+
+        expenseListView = (ListView) view.findViewById(R.id.expenseListView);
 
         newEntryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +55,8 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        populateListView();
+
         return view;
     }
 
@@ -46,5 +65,26 @@ public class HomeFragment extends Fragment {
         Log.d(TAG, "onDestroyView: ");
         super.onDestroyView();
         view = null;
+    }
+
+    public void populateListView() {
+        // Get expense and append to ArrayList
+        Cursor cursor = databaseHandler.getExpense();
+        ArrayList<String> expenseList = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            // Get value from column 1 and add to List
+            expenseList.add(cursor.getString(1));
+        }
+        // Create and set List Adapter
+        ListAdapter listAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, expenseList);
+        expenseListView.setAdapter(listAdapter);
+
+        // Add onItemClickListener to ListView
+        expenseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = adapterView.getItemAtPosition(i).toString();
+            }
+        });
     }
 }
--- app/src/main/java/com/yujotseng/expenselogger/NewEntryFragment.java
+++ app/src/main/java/com/yujotseng/expenselogger/NewEntryFragment.java
@@ -3,20 +3,52 @@ package com.yujotseng.expenselogger;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewEntryFragment extends Fragment {
     private static final String TAG = "NewEntryFragment";
     
     private View view;
+    private Button saveButton;
+    private EditText expenseName;
+    private DatabaseHandler databaseHandler;
+    private Fragment fragment;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.new_entry_layout, container, false);
+
+        // Instantiate database
+        databaseHandler = new DatabaseHandler(getActivity(), null, null, 1);
+
+        saveButton = (Button) view.findViewById(R.id.saveButton);
+
+        expenseName = (EditText) view.findViewById(R.id.expenseName);
+
+        fragment = getFragmentManager().findFragmentByTag("HomeFragment");
+
+        if(fragment == null) {
+            fragment = new HomeFragment();
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add expense to database and return to HomeFragment
+                saveButtonClicked();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentContainer, fragment);
+                transaction.commit();
+            }
+        });
 
         return view;
     }
@@ -25,5 +57,18 @@ public class NewEntryFragment extends Fragment {
         Log.d(TAG, "onDestroyView: ");
         super.onDestroyView();
         view = null;
+    }
+
+    private void saveButtonClicked() {
+        if (expenseName.length() != 0) {
+            Expense expense = new Expense(expenseName.getText().toString());
+            if (expense != null) {
+                databaseHandler.addExpense(expense);
+            } else {
+                Toast.makeText(getActivity(),"Object null", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getActivity(),"You must put something in the text field!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
